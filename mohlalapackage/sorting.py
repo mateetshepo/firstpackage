a6e2fa6ea0fd6489bfda68e59f88b9d935b62343
Mmatshepho Mohlala <matshepophurutsi@gmail.com>
2019-03-20 15:49:12 +0200

    final commit

--- mohlalapackage/sorting.py
+++ mohlalapackage/sorting.py
@@ -1,36 +1,63 @@
-def sum_array(array):
+def bubble_sort(items):
 
-    ''' Return sum of all items in array'''
-    answer = 0
-    for x in array:
-        answer += x
-    return answer
+    '''Return array of items, sorted in ascending order'''
+    for a in range(0, len(items)-1):
+        for b in range(0, len(items-1-a)):
+            if items[b]>items[b+1]:
+                items[b], items[b+1] = items[b+1], items[b]
+    return items
 
 
-def fibonacci(n):
+def merge_sort(items):
 
-    '''Return nth term in fibonacci sequence'''
-    if n == 0:
-        return 0
-    elif n==1:
-        return 1
+    """Return array of items, sorted in ascending order using merge sort"""
+    if len(items) >1:
+        mid = len(items)//2
+        left = items[:mid]
+        right = items[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i=0
+        j=0
+        k=0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                items[k] = left[i]
+                i+=1
+            else:
+                items[k] = right[j]
+                j+=1
+            k+=1
+
+        while i < len(left):
+            items[k] = left[i]
+            i+=1
+            k+=1
+
+        while j < len(right):
+            items[k] = right[j]
+            j+=1
+            k+=1
+
+    return items
+
+
+def quick_sort(items):
+    '''Return array of items, sorted in ascending order'''
+    if len(items) == 1 or len(items) == 0:
+        return items
     else:
-        return fibonacci(n-1)+fibonacci(n-2)
-
-
-def factorial(n):
-
-    '''Return n!'''
-    num = 1
-    while num >= 1:
-        num = num * n
-        n = n-1
-    return num
-
-
-def reverse(word):
-    '''Return word in reverse'''
-    if len(word) > 1:
-        return word[-1] + reverse(word[:-1])
-    else:
-        return word
+        pivot = items[0]
+        i = 0
+        for j in range(len(items)-1):
+            if items[j+1] < pivot:
+                items[j+1],items[i+1] = items[i+1], items[j+1]
+                i += 1
+        items[0],items[i] = items[i],items[0]
+        first_part = quick_sort(items[:i])
+        second_part = quick_sort(items[i+1:])
+        first_part.append(items[i])
+        return first_part + second_part
